7cc5dd19b3a4b82132e95e00087da9fdfad98232
David Alzate <illker@hotmail.com>
2021-03-11 21:51:06 +0000

    Insert right :fast_forward:

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right -  inserts a node as the right-child of another node
+ * @parent: pointer to the node insert
+ * @value:  value to store
+ * Return: Pointer to the created node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *burger_node = NULL;
+
+	if (!parent)
+		return (NULL);
+	burger_node = malloc(sizeof(binary_tree_t));
+	if (!burger_node)
+		return (NULL);
+
+	burger_node->parent = parent;
+	burger_node->n = value;
+	burger_node->left = NULL;
+
+	if (parent->right)
+	{
+		burger_node->right = parent->right;
+		parent->right = burger_node;
+	}
+	else
+	{
+		parent->right = burger_node;
+		burger_node->right = NULL;
+	}
+	return (burger_node);
+}
